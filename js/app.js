1fb381ab8d3d2525010b01c3177eed384d1e1492
Quintin Kelly Dekker <quintinkellydekker888@gmail.com>
2023-02-19 10:36:12 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 /// ********SPINNING LOGO**********//
 // ********************************//
 // ********************************//
-
 const logo = document.querySelector('.logo');
 
 let rotation = 0;
@@ -14,20 +13,18 @@ setInterval(function() {
 // *******************************//
 // *************NAV***************//
 
-const nav=document.querySelector(".nav");
-const linkText=["Home |",
-"About Us |",
-"Videos |",
-"Shop |",
-"Contact |"];
-const linkContainer=document.querySelector(".link-container");
+const nav = document.querySelector(".nav");
+const linkText = ["Home |", "About Us |", "Videos |", "Shop |", "Contact |"];
+const hrefValues = ["#home", "#about", "#videos", "#shop", "#contact"];
+const linkContainer = document.querySelector(".link-container");
 
-for (let i=0; i < linkText.length; i++) {
-  const link=document.createElement("a");
-  link.href="#about-container"+linkText[i];
-  link.textContent=linkText[i];
+for (let i = 0; i < linkText.length; i++) {
+  const link = document.createElement("a");
+  link.textContent = linkText[i];
+  link.setAttribute("href", hrefValues[i]);
   linkContainer.appendChild(link);
 }
+
 
 // *************MOBILE NAV BTN*****************//
 
@@ -48,38 +45,34 @@ for (let i=0; i < contentArray.length; i++) {
 // *******************************//
 // *******************************//
 
-document.addEventListener('DOMContentLoaded', ()=> {
-    const mbNav=document.querySelector('.mobile-nav');
-    const mobileArray=['Home', 'About Us', 'Videos', 'Shop', 'Contact'];
+document.addEventListener('DOMContentLoaded', () => {
+  const mbNav = document.querySelector('.mobile-nav');
+  const mobileArray = ['Home', 'About Us', 'Videos', 'Shop', 'Contact'];
+  const hrefArray = ['#home', '#about', '#videos', '#shop', '#contact']; // New array with href values
 
-    for (let i=0; i < mobileArray.length; i++) {
-      const mLinks=document.createElement('a');
-      mLinks.textContent=mobileArray[i];
-      mLinks.href='#'+ mobileArray[i];
-      mbNav.appendChild(mLinks);
-    }
-
-    const mbNavBtn=document.querySelector('.mobile-nav-btn');
-
-    mbNavBtn.addEventListener('click', ()=> {
-        console.log("nav click");
-        mbNavBtn.classList.toggle('show-mobile-nav');
-      }
-
-    );
-
-    document.body.addEventListener('click', (e)=> {
-        console.log("body click");
-
-        if ( !mbNavBtn.contains(e.target)) {
-          mbNavBtn.classList.remove('show-mobile-nav');
-        }
-      }
-
-    );
+  for (let i = 0; i < mobileArray.length; i++) {
+    const mLinks = document.createElement('a');
+    mLinks.textContent = mobileArray[i];
+    mLinks.href = hrefArray[i]; // Set the href attribute using the new array
+    mbNav.appendChild(mLinks);
   }
 
-);
+  const mbNavBtn = document.querySelector('.mobile-nav-btn');
+
+  mbNavBtn.addEventListener('click', () => {
+    console.log("nav click");
+    mbNavBtn.classList.toggle('show-mobile-nav');
+  });
+
+  document.body.addEventListener('click', (e) => {
+    console.log("body click");
+
+    if (!mbNavBtn.contains(e.target)) {
+      mbNavBtn.classList.remove('show-mobile-nav');
+    }
+  });
+});
+
 
 // HERO***************************//
 // *******************************//
@@ -88,22 +81,23 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
 const hero=document.querySelector(".hero");
 const heading=document.createElement("h1");
-const h3 = document.createElement("h3");
-const h2 = document.createElement("h2");
-const h5 = document.createElement("h5");
-const subBtn1 = document.createElement("button");
+const h3=document.createElement("h3");
+const h2=document.createElement("h2");
+const h5=document.createElement("h5");
+const subBtn1=document.createElement("button");
 const para=document.createElement("p");
-const sDBtn = document.createElement("down-button");
+const sDBtn=document.createElement("a");
 
 
 heading.textContent="Welcome";
-h3.textContent = "To";
-h2.textContent = "African";
-h5.textContent = "Trails & Tails"
-subBtn1.textContent = "Subscribe";
+h3.textContent="To";
+h2.textContent="African";
+h5.textContent="Trails & Tails"
+subBtn1.textContent="Subscribe";
 para.textContent="From the mountains to the beaches, we've got it all covered on our travel channel..";
-sDBtn.textContent = "+";
-sDBtn.href = "#about-container";
+sDBtn.textContent="+";
+sDBtn.href="#about";
+sDBtn.class="scroll-down-button";
 
 
 
@@ -142,3 +136,25 @@ aboutContainer.appendChild(aboutUs);
 const aboutPara=document.createElement("p");
 aboutPara.textContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas amet id tenetur tempore officiis repudiandae nisi excepturi eius, soluta quidem distinctio possimus temporibus dicta accusantium aliquam eveniet iure iste, corrupti omnis! Maiores adipisci perferendis quis ab qui sapiente, a tempora sint sunt possimus ratione consectetur eum, magni velit atque provident. Dolorum aliquam perspiciatis distinctio! Doloribus facilis officiis quia in iste saepe magni excepturi labore aliquid ducimus ea officia rerum veritatis facere eos ipsum nulla delectus minus libero similique, repellendus explicabo modi asperiores. Commodi voluptatem reprehenderit, corrupti provident laborum doloremque aut nobis facere placeat? Hic odit iusto architecto aliquam molestiae voluptatem iste officiis maxime deleniti esse. Autem ad quaerat nesciunt porro vel. Soluta asperiores vero nostrum, voluptatem voluptas perspiciatis nihil. Asperiores.";
 aboutContainer.appendChild(aboutPara);
+
+// *******************************//
+// *******************************//
+// *****************************//// **********************************//
+// ********BACK TO TOP BTN********//
+
+const backToTopBtn = document.querySelector('#back-to-top-btn');
+
+window.onscroll = function() {
+  if (window.pageYOffset > 20) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+};
+
+backToTopBtn.addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
